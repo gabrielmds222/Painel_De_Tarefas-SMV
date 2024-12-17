1c23844e7c3ad62d96f21ead5415ee7752ecd6e3
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-12-17 14:38:16 -0300

    feat: Criação do projeto frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Principal from "./principal/page";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Principal />
+    </div>
+  );
+}
---
+++ frontend/src/app/principal/page.tsx
@@ -0,0 +1,7 @@
+export default function Principal() {
+  return (
+    <div>
+      <h1>Teste da página</h1>
+    </div>
+  );
+}
