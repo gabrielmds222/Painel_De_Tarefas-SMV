a3345cf7653e17fa7d24d63eaa2fc7dea935c4d2
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-12-22 22:15:52 -0300

    Correção de estilização

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,12 +7,58 @@ export default async function Home() {
   console.log(tarefas);
 
   return (
-    <main className="max-w-4xl mx-auto mt-4">
-      <div className="text-center my-5 flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Painel de Tarefas</h1>
-        <BtnAdicionaTarefa />
+    <>
+      <header className="w-full px-6 py-4 flex items-center">
+        <h1 className="text-xl font-bold mx-auto">Painel de Tarefas</h1>
+      </header>
+
+      <div className="max-w-5xl mx-auto mt-4 flex flex-col gap-4">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-4">
+            <label>
+              <input
+                type="radio"
+                name="priority-filter"
+                value="alta"
+                className="radio radio-primary"
+              />
+              <span className="ml-2">Alta</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="priority-filter"
+                value="media"
+                className="radio radio-primary"
+              />
+              <span className="ml-2">Média</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="priority-filter"
+                value="baixa"
+                className="radio radio-primary"
+              />
+              <span className="ml-2">Baixa</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="priority-filter"
+                value="todas"
+                className="radio radio-primary"
+              />
+              <span className="ml-2">Todas</span>
+            </label>
+          </div>
+          <BtnAdicionaTarefa />
+        </div>
       </div>
-      <Tabela tarefas={tarefas} />
-    </main>
+
+      <main className="max-w-5xl mx-auto mt-4">
+        <Tabela tarefas={tarefas} />
+      </main>
+    </>
   );
 }
